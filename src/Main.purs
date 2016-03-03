1daada72ad0a8cfec8c311e732dc86bd2cb40d04
Mateusz Wolsza <matmaw@gmail.com>
2016-03-03 22:12:09 +0100

    Step 1
    Run simple HTTP server

---
+++ src/Main.purs
@@ -0,0 +1,20 @@
+module Main where
+
+import Prelude
+
+import Node.HTTP
+import Node.Stream
+import Node.Encoding
+
+import Data.Foldable (foldMap)
+
+import Control.Monad.Eff.Console
+
+
+main = do
+  server <- createServer respond -- (1)
+  listen server 8080 $ void do   -- (2)
+    log "Server is listening!"
+  where
+  respond req res = do           -- (3)
+    log ( "Incoming: " <> requestMethod req <> " " <> requestURL req)
